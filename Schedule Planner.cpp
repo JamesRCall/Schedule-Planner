ea977aa4c0fec7af3dd4f0d06f5a5bdea893b4d7
WolfXae <113481301+WolfXae@users.noreply.github.com>
2023-02-22 19:59:46 -0600

    Add files via upload
    minor changes, added comments, striking error with other toggle again...

--- Schedule Planner.cpp
+++ Schedule Planner.cpp
@@ -29,10 +29,10 @@ int main()
     
     do{
         cout << "alright all set let me just make sure everything is correct..." << endl;
-        cout << "you are in " << Classes << "classes." << endl;
-        cout << "you are in " << Clubs << "clubs." << endl;
+        cout << "you are in " << Classes << " classes." << endl;
+        cout << "you are in " << Clubs << " clubs." << endl;
         if (Other_Enabled == 1){
-            cout << "you are in " << Other << "events." << endl;
+            cout << "you are in " << Other << " events." << endl;
         }
         cout << "Is this correct? (y/n)" << endl;  
         if (YesOrNo(check) == 0){
@@ -57,6 +57,32 @@ int main()
             toggle_fix = 0;
         }
     }while(toggle_fix == 1);
+    cout << "All done! time to make your schedule..." << endl;
+/*
+---------------------------------------TODO--------------------------------------
+1. ask user for times of each class and frequency
+2. ask user for times of each club and frequency
+3. if user has events ask for times of each and frequency
+4. ask user what time they sleep and wake up
+5. ask user what times they eat
+6. find what time periods are left
+7. create a schedule for user
+8. create multiple schedules for user to choose from
+9. optimize schedule to user preferences
+10. optimize study hours based on class difficulty
+11. create breaks and free times based on user preference
+12. reward system????
+13. meal plan?????
+14. encouraging phrases
+15. allow variation in messages
+16. create save files for users????
+17. create front facing application
+18. optimize front facing application
+19. track users progress
+20. optimize schedule based user progress
+21. actually add comments to my code lmao
+-------------------------------------------------------------------------------------
+*/
 
     return 0;
 
